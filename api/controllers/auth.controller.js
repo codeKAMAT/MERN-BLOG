ea498d8af4bed72fa5279ff7efc4d122d357a8a7
RagaAI_User <ragaai_user@RagaAI-Users-MacBook-Pro.local>
2024-04-15 10:49:38 +0530

    add middleware to handle error

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,26 +1,29 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs"
+import { errorHandler } from "../utils/error.js";
 
-export const signup = async ( req, res ) => {
+export const signup = async ( req, res, next) => {
     const {username, email, password} = req.body;
 
     if(!username || !email || !password || username === '' || password === '' || email ===  '' ) {
-        return res.status(400).json({message: "All field required"});
+         next(errorHandler(400, 'All Fields are required'))
+    
     }
+    
 
     const hashedPassword = bcryptjs.hashSync(password, 10);
 
     const newUser = new User({
         username,
+        email,
         password: hashedPassword,
-        email
     });
 
     try {
         await newUser.save();
         res.json("Signup successful")
     } catch (error) {
-        res.status(500).json({message: error.message})
+        next(error)
     }
   
 }
--- api/index.js
+++ api/index.js
@@ -16,8 +16,8 @@ console.log("MOngoDB connected");
 
 const app = express();
 
-//allow json
-app.use(express.json());
+
+app.use(express.json()); //allow json
 
 app.listen(6000, () => {
     console.log('Server is running on port 6000!!!!');
@@ -25,3 +25,15 @@ app.listen(6000, () => {
   
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
+
+
+//middleware to handle error
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message
+    })
+})
